f5aeaaf14bc727097b293c9d990048d358c7ccff
veezo101 <aviral109@hotmail.com>
2023-09-30 14:27:10 +0400

    Mute and Reset functionality completed

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import webbrowser
 # from magic import *
 import tkinter as tk
 from tkinter import ttk
+import os
+import zipfile
 from tkinterdnd2 import DND_FILES, TkinterDnD
 
 class magic:
@@ -18,13 +20,97 @@ class magic:
             statusLabel.config(text="Updated {0} to {1}".format("479",id))
         except IOError:
             statusLabel.config(text="Err Invalid File")
+    
+    def sfxGetCurrentState():
+        isExistSFX = os.path.exists('{0}/SFX'.format(path))
+        isExistsRotoOG = os.path.exists('{0}/SFX-RotoOG'.format(path))
+        isExistsRotoSilent = os.path.exists('{0}/SFX-RotoSilent'.format(path))
+        if(isExistSFX == True and isExistsRotoSilent == False and isExistsRotoOG==True):
+            return "muted"
+        elif(isExistSFX == True and isExistsRotoSilent == False and isExistsRotoOG==False):
+            return "clean"
+        elif(isExistSFX == True and isExistsRotoSilent == True and isExistsRotoOG==False):
+            return "unmuted"
+        elif(isExistSFX==True and  isExistsRotoSilent == True and isExistsRotoOG==True):
+            return "schrodinger"
+        elif(isExistSFX==False and isExistsRotoOG==True):
+            return "nosfxbutog"
+        else:
+            return "raiseResetFlag"
+    
+    def sfxResetFolder():
+        try:
+            if(os.path.exists('{0}/SFX-RotoSilent'.format(path))):
+                shutil.rmtree('{0}/SFX-RotoSilent'.format(path),ignore_errors=True)
+            if(os.path.exists('{0}/SFX-RotoOG'.format(path))):
+                if(os.path.exists('{0}/SFX'.format(path))):
+                    shutil.rmtree('{0}/SFX'.format(path),ignore_errors=True)
+                os.rename("{0}/SFX-RotoOG".format(path),"{0}/SFX".format(path))
+            else:
+                if(os.path.exists('{0}/SFX'.format(path))==False):
+                    os.mkdir('{0}/SFX'.format(path))
+            statusLabel.config(text="Successfully Restored SFX directory structure")
+            SFXCurrentModeStatusLabel.config(text=magic.sfxGetCurrentState())
+        except Exception as ex:
+            statusLabel.config(text="Error: {0} args: {1}".format(type(ex).__name__,ex.args))
+
+    def sfxHouseKeeping():
+        isExistSFX = os.path.exists('{0}/SFX'.format(path))
+        isExistsRotoOG = os.path.exists('{0}/SFX-RotoOG'.format(path))
+        isExistsRotoSilent = os.path.exists('{0}/SFX-RotoSilent'.format(path))
+        #debug
+        SFXCurrentModeStatusLabel.config(text=magic.sfxGetCurrentState())
+    def mutePmu():
+        try:
+            currentState = magic.sfxGetCurrentState()
+            if(currentState=="muted"):
+                statusLabel.config(text="Already muted!")
+                return
+            
+            if(currentState == "raiseResetFlag" or currentState=="schrodinger" or currentState=="nosfxbutog"):
+                magic.sfxResetFolder()
+                statusLabel.config(text="Folders Reset due to an error. Please try again")
+
+            if(currentState=="clean"):
+                with zipfile.ZipFile('./SilentSFX.zip','r') as silent_zip:
+                    silent_zip.extractall('{0}/SFX-RotoSilent'.format(path))
+                os.rename("{0}/SFX".format(path),"{0}/SFX-RotoOG".format(path))
+                os.rename("{0}/SFX-RotoSilent".format(path),"{0}/SFX".format(path))
+                statusLabel.config(text="Client successfully muted")
+
+            if(currentState=="unmuted"):
+                os.rename("{0}/SFX".format(path),"{0}/SFX-RotoOG".format(path))
+                statusLabel.config(text="Moved current to OG")
+                os.rename("{0}/SFX-RotoSilent".format(path),"{0}/SFX".format(path))
+                statusLabel.config(text="Client successfully muted")
+            SFXCurrentModeStatusLabel.config(text=magic.sfxGetCurrentState())
+
+            # magic.sfxHouseKeeping()
+            
+        except FileNotFoundError as ex:
+            # statusLabel.config(text="Failed to find folders (SFX-RotoOG or SFX-RotoSilent)")
+            statusLabel.config(text="Error: {0} args: {1}".format(type(ex).__name__,ex.args))
+        except Exception as ex:
+            statusLabel.config(text="Failed to mute client. Try again after closing the Client")
+            #debug
+            #statusLabel.config(text="Error: {0} args: {1}".format(type(ex).__name__,ex.args))
+
+    def unMutePmu():
+        try:
+            os.rename("{0}/SFX".format(path),"{0}/SFX-RotoSilent".format(path))
+            os.rename("{0}/SFX-RotoOG".format(path),"{0}/SFX".format(path))
+            statusLabel.config(text="Client successfully unmuted!")
+        except:
+            statusLabel.config(text="Failed to unmute client. Try again after closing the Client")
+        
+
 
 try:
     with open('path.txt', 'r') as file:
         path = file.read().rstrip()
     root= tk.Tk()
     root.iconbitmap("rotoview.ico")
-    root.title('Rotoview-v0.2.2')
+    root.title('Rotoview-v0.2.3')
     root.geometry("600x400")
 
     #staus label
@@ -90,6 +176,21 @@ try:
 
     #tab 3 - SFX
     tab3 = ttk.Frame(notebook)
+
+    ##SFX Buttons
+    SFXMuteButton = tk.Button(tab3,text='Enable Silent Client', command=magic.mutePmu, bg='brown',fg='white',takefocus=False)
+    SFXUnMuteButton = tk.Button(tab3,text='Disable Silent Client', command=magic.unMutePmu, bg='brown',fg='white',takefocus=False)
+    SFXResetButton = tk.Button(tab3,text='Reset SFX Folders', command=magic.sfxResetFolder, bg='brown',fg='white',takefocus=False)
+
+    SFXMuteButton.place(relx=0.39,rely=0.45,anchor="center")
+    SFXUnMuteButton.place(relx=0.61,rely=0.45,anchor="center")
+    SFXResetButton.place(relx=0.50,rely=0.82,anchor="center")
+
+    ##Current Mode Labels
+    SFXCurrentModeLabel = tk.Label(tab3,text="Current Mode: ", fg='blue', font=('helvetica', 12, 'bold'))
+    SFXCurrentModeStatusLabel = tk.Label(tab3,text=magic.sfxGetCurrentState(), fg='orange red', font=('helvetica', 12, 'bold'))
+    SFXCurrentModeLabel.place(relx=0.39,rely=0.65,anchor="center")
+    SFXCurrentModeStatusLabel.place(relx=0.55,rely=0.65,anchor="center")
 
     
     notebook.add(tab1,text='Start up')
