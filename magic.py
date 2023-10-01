cced205f17d1367acde8b94906e1882e77d2413d
veezo101 <aviral109@hotmail.com>
2023-10-01 17:06:13 +0400

    removed old project files

--- magic.py
+++
@@ -1,7 +0,0 @@
-import shutil
-
-class magic:
-    def restore():
-        shutil.copy("./notouch/Chara-479.chara","./Chara-479.chara")
-    def replace():
-        shutil.copy("owo.txt","./Chara-479.chara")
